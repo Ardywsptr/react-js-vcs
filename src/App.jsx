95f5df5fe03c6c581ec8ccc46f19685bcbccacfc
Ardy wirasaputra <ardywsptr1@gmail.com>
2023-10-25 15:58:52 +0700

    component & props

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function Button({ variant, text }) {
+  return (
+    <button className={`h-10 px-6 font-semibold rounded-md ${variant ? variant : "bg-green-700"} text-white`} type="submit">
+      {text ? text : "Submit"}
+    </button>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex justify-center bg-blue-600 items-center min-h-screen">
+        <div className="flex gap-x-2">
+          <Button variant="bg-red-700" text="Login" />
+          <Button variant="bg-slate-700" text="Logout" />
+          <Button variant="bg-black" text="Register" />
+          <Button />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
