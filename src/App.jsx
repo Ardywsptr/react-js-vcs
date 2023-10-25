e30ae5d6bc161941d8127c562f4ad12df62040b0
Ardy wirasaputra <ardywsptr1@gmail.com>
2023-10-25 16:17:31 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import RegisterPage from "./Pages/register";
+import LoginPage from "./Pages/login";
 
 function App() {
   return (
     <>
       <div className="flex justify-center items-center min-h-screen">
-        {/* <LoginPage /> */}
-        <RegisterPage />
+        <LoginPage />
+        {/* <RegisterPage /> */}
       </div>
     </>
   )
