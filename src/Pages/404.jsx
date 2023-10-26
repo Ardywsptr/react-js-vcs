3f50d693c236314745b97c25c5463d0af7c31720
Ardy wirasaputra <ardywsptr1@gmail.com>
2023-10-26 09:57:46 +0700

    Update 404.jsx

--- src/Pages/404.jsx
+++ src/Pages/404.jsx
@@ -2,12 +2,13 @@ import { useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
     const error = useRouteError();
+    console.log(error);
 
     return (
         <div className="flex justify-center items-center min-h-screen flex-col">
             <h1 className="text-3xl font-bold">Oops!</h1>
             <p className="my-5 text-xl">Sorry, an unexpected error has occurred.</p>
-            <p className="text-lg">{error.statusText || error.message}</p>
+            <p className="text-lg">{error.status} : {error.statusText || error.message}</p>
         </div>
     )
 }
